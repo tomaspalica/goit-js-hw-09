897c57b87083107092b024e129c7c9d77e92cfc6
tomaspalica <tomasss.palica@gmail.com>
2023-07-02 12:44:24 +0200

    switched from cdn to npm

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
-
-
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 const timePicker = document.querySelector("#datetime-picker")
 const btnPicker = document.querySelector("button[data-start]")
 const timerDays = document.querySelector("span[data-days]")
